9ebb4ae834ec017067b817d3b502da39d32716b3
OVENGEE <mpumelelokenneth911@gmail.com>
2025-08-17 17:37:55 +0200

    Greyboxing , UI and mechanics
    For the grey boxing, I added two new games or rather, made them more shaped and detailed.  On the UI there is a progress bar responsible for telling you how many tickets you have collected. On the mechanics I have added code that allows you to collect objects.

---
+++ 3D Unity Game Project/Assets/Scripts/Ticket.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+public class Ticket : MonoBehaviour, IItem
+{
+    public static event Action<int> OnTicketCollect; // Event to notify when a gem is collected
+    public int worth = 5;
+    public void Collect()
+    {
+        OnTicketCollect.Invoke(worth); // Invoke the event with the gem's worth
+        
+        Destroy(gameObject); // Destroy the gem GameObject when collected
+    }
+}
