2f80fb2663f3097558aa50bf7432fb3a2e7ada82
Caseiin <leruopono@gmail.com>
2025-08-17 14:06:30 +0200

    DevScene| Quit interfaces completed
    The quit interfaces works added code to make the application quit was used to make it quit in the unity editor.

--- 3D Unity Game Project/Assets/Scripts/UI/Overlay/QuitApplication.cs
+++ 3D Unity Game Project/Assets/Scripts/UI/Overlay/QuitApplication.cs
@@ -30,17 +30,6 @@ public class QuitApplication : MonoBehaviour
     }
 
 
-    void Update()
-    {
-        if (EventSystem.current.IsPointerOverGameObject())
-        {
-            Cursor.visible = true;
-        }
-        else
-        {
-            Cursor.visible = false;
-        }
-    }
     void OnEnable()
     {
         inputActions.UI.Enable();
@@ -57,17 +46,33 @@ public class QuitApplication : MonoBehaviour
     private void OnQuitTrigger(InputAction.CallbackContext context)
     {
         quitMenu.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 
     public void CancelQuitMenu()
     {
         quitMenu.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-
 }
+
+// Code references:
+// 1)Title: Cursor visibility and lockstates
+//  Author: Chatpgt
+//  Date accessed:  17/08/2025
+//  Availability: https://chatgpt.com/c/68a1baed-8408-8330-b836-79bdfe824920
+
+// Chatgpt was used to help me understand and debug why I could not interact with my button even tho I was clicking it
+// The issue was when I clicked the cursor would become invisible and return to the middle of the screent instead of interacting with the button!
