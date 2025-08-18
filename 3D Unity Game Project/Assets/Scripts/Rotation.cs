bedd9481dfb093dca1f9da0f4432e2c46ca7109d
OVENGEE <mpumelelokenneth911@gmail.com>
2025-08-18 19:29:26 +0200

    Materials
    More color and materials

--- 3D Unity Game Project/Assets/Scripts/Rotation.cs
+++ 3D Unity Game Project/Assets/Scripts/Rotation.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-   
+    public float rotationSpeed = -0.5f;
     void Update()
     {
-        transform.Rotate(0, -0.2f, 0);
+        transform.Rotate(0, rotationSpeed, 0);
     }
 }
