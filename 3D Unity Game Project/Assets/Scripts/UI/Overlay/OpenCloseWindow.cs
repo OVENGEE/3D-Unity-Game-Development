da4679b7e9bc2a7545b567f2175be479ddb1c5f3
Caseiin <leruopono@gmail.com>
2025-08-28 16:39:58 +0200

    Assets| UI tweening framework

---
+++ 3D Unity Game Project/Assets/Scripts/UI/Overlay/OpenCloseWindow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class OpenCloseWindow : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//  Code references:
+// 1) Title:How to animate your UI with code in Unity | UI Tweening
+//    Author:Christina Creates Games
+//    Date: 28/08/2025
+//    Availiability: https://www.youtube.com/watch?v=AaudFyM3KV0
+
+//This gave me the understanding of the basics of tweening and how to make use of move event based code .
