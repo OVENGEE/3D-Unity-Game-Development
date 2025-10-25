3d7a388278d4c74d757dda28155b8713d27d6995
Caseiin <leruopono@gmail.com>
2025-10-25 22:55:50 +0200

    TutorialManager.cs| Tutorial logic
    The tutorial video player works perfectly I just need to have a description for each  video played

--- 3D Unity Game Project/Assets/Scripts/UI/Overlay/Tutorials/TutorialManager.cs
+++ 3D Unity Game Project/Assets/Scripts/UI/Overlay/Tutorials/TutorialManager.cs
@@ -7,27 +7,40 @@ using UnityEngine.Windows.WebCam;
 
 public class TutorialManager : MonoBehaviour
 {
+    //Video declarations
+    [Header("Video Requirements")]
+    [SerializeField]private RawImage tutorialScreen;
+    private VideoPlayer videoPlayer;
     //Tutorial declarations
+    [Header("Tutorial Library")]
     public TutorialLibrary[] tutorialLibraries;
     private Dictionary<TutorialType, TutorialLibrary> tutorialMap;
 
     private TutorialLibrary currentTutorial;
 
-    //Video declarations
-    private RawImage tutorialScreen;
-    private VideoPlayer videoPlayer;
+
 
     private void Awake()
     {
-        tutorialScreen = GetComponentInChildren<RawImage>();
-        videoPlayer = tutorialScreen.GetComponent<VideoPlayer>();
+        if (tutorialScreen == null)
+        {
+            Debug.LogError("TutorialManager: no RawImage found in scene.");
+            return;
+        }
+
+        videoPlayer = tutorialScreen?.GetComponent<VideoPlayer>();
 
         ////Build the dictionary for quick access
         tutorialMap = new Dictionary<TutorialType, TutorialLibrary>();
         foreach (var tutorialInfo in tutorialLibraries)
         {
             if (!tutorialMap.ContainsKey(tutorialInfo.tutorialName))
+            {
                 tutorialMap.Add(tutorialInfo.tutorialName, tutorialInfo);
+                Debug.Log($"{tutorialInfo.tutorialName} tutorial added");
+            }
+                
+
         }
     }
 
@@ -43,6 +56,7 @@ public class TutorialManager : MonoBehaviour
 
     private void PlayVideo(VideoClip clip)
     {
+        Debug.Log($"{clip} is being played");
         videoPlayer.clip = clip;
         videoPlayer.Play();
     }
@@ -50,6 +64,7 @@ public class TutorialManager : MonoBehaviour
     private void TutorialPlay(TutorialType tutorial)
     {
         currentTutorial = tutorialMap[tutorial];
+        Debug.Log($"Current tutorial: {tutorial}");
         if (!currentTutorial.played)
         {
             var currentTutClip = currentTutorial.tutorialClip;
