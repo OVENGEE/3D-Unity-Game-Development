9a256820f64361521e5e26854a934bd2555d366c
OVENGEE <mpumelelokenneth911@gmail.com>
2025-11-09 06:36:24 +0200

    Modifying Entrance and lights
    Adding emission to the entrance letters and a flickering lights script

---
+++ 3D Unity Game Project/Assets/Scripts/Light/FlickeringLights.cs
@@ -0,0 +1,44 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(Light))]
+
+public class FlickeringLights : MonoBehaviour
+{
+
+    private Light lightToFlicker;
+    [SerializeField, Range(0f, 3f)] private float minIntensity = 0.5f;
+    [SerializeField, Range(0f, 3f)] private float maxIntensity = 1.2f;
+    [SerializeField, Min(0f)] private float timeBetweenIntensity = 0.1f;
+
+    private float currentTimer;
+
+    private void Awake()
+    {
+        if (lightToFlicker == null)
+        {
+            lightToFlicker = GetComponent<Light>();
+        }
+
+        ValidateIntensityBounds();
+    }
+
+    private void Update()
+    {
+        currentTimer += Time.deltaTime;
+        if (!(currentTimer >= timeBetweenIntensity)) return;
+        lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer = 0;
+    }
+
+    private void ValidateIntensityBounds()
+    {
+        if (!(minIntensity > maxIntensity))
+        {
+            return;
+        }
+        Debug.LogWarning("Min Intensity is greater than max Intensity, swapping values!");
+        (minIntensity, maxIntensity) = (maxIntensity, minIntensity);
+    }
+}
